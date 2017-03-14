619a89ba2de4dbddbd19a3f561a95a9ca640016c
armi1401 <armi1401@gmail.com>
2017-03-14 13:47:53 +0100

    Added posts controller, views and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only:[:index]
-	before_action :set_user, only:[:show]
+  before_action :set_user, only:[:show]
 
-	def index
-		case params[:people] when "friends"
+  def index
+    case params[:people] when "friends"
       @users = current_user.active_friends
     when "requests"
       @users = current_user.pending_friend_requests_from.map(&:user)
@@ -12,15 +12,18 @@ class UsersController < ApplicationController
     else
       @users = User.where.not(id: current_user.id)
     end
-	end
-
-  def show
-    @activities = PublicActivity::Activity.where(owner_id: @user.id) + PublicActivity::Activity.where(recipient_id: @user.id)
   end
 
-  private 
+  def show
+    @post = Post.new
+    @posts = @user.posts
+    @posts = @user.posts.order('created_at DESC')
+    @activities = PublicActivity::Activity.where(owner_id: @user.id).order('created_at DESC')
+  end
+
+  private
 
   def set_user
-  	@user = User.find_by(username: params[:id])
+    @user = User.find_by(username: params[:id])
   end
 end
